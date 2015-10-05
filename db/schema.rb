59777953aceb27ab51c64372c090205cc03b815d
abs <abs@abss-MacBook-Pro.local>
2015-10-05 02:24:46 +0100

    add quantity column to Products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151005010929) do
+ActiveRecord::Schema.define(version: 20151005012403) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151005010929) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "image"
+    t.integer  "quantity"
   end
 
   create_table "purchases", force: :cascade do |t|
